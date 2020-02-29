7a060d6fafdc447dd6167dd0655450e7bb02882b
玄苍 <xuanlantiancang@users.noreply.github.com>
2020-02-29 10:29:17 +0800

    Initial commit

---
+++ app/src/main/java/com/xuancang/emotion/ui/List/List.java
@@ -0,0 +1,16 @@
+package com.xuancang.emotion.ui.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.xuancang.emotion.R;
+
+public class List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
